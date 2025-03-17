6e925a24120f74745fcbf62953efdd951d6dd3ad
Your Name <you@example.com>
2025-03-17 21:55:08 +0530

    add code

---
+++ src/Employdata.js
@@ -0,0 +1,32 @@
+const Employdata = [
+    {
+        id: 1,
+        firstname: "John",
+        lastname: "Doe",
+        age : 25,
+        salary: 50000,
+        department: "IT"  
+        
+    }, 
+
+    {
+        id: 2,
+        firstname: "Jane",
+        lastname: "Doe",
+        age : 30,
+        salary: 60000,
+        department: "HR"
+    },
+
+    {
+        id: 3,
+        firstname: "Jill",
+        lastname: "Doe",
+        age : 35,
+        salary: 70000,
+        department: "Finance"
+    }
+
+   
+]
+export default Employdata;

a8bad389c4f69b7ed2c55c8ca1531ad618a65856
Your Name <you@example.com>
2025-03-19 21:39:10 +0530

    add code

--- src/Componet/Home.jsx
+++ src/Componet/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState  } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
@@ -17,6 +18,12 @@ const Home = () => {
             <div className="home fixed w-full h-full">
                 <div className="container mx-auto items-center justify-center  h-screen">
                     <h1 className="text-[45px] font-semibold text-center py-4 px-5">React Crude</h1>
+                    <div className="add flex justify-end px-5 py-5 "> 
+                    <Link to="/create" className="bg-slate-500 p-2 text-white font-semibold w-28 text-center text-lg">ADD +</Link>  
+
+                    </div>
+                        
+                    
                         <table className="table-auto w-full 
                         border-collapse border border-gray-400">
                             <thead>
@@ -28,8 +35,7 @@ const Home = () => {
                                     <th className="border-s-4">phone</th>
                                     <th className="border-s-4">website</th>
                                     <th className="border-s-4">Button</th>
-
-                                </tr>
+                                 </tr>
                             </thead>
                             <tbody className="border border-gray-400 text-center  ">
                                 {
@@ -42,19 +48,18 @@ const Home = () => {
                                         <td className="border-s-4">{d.phone}</td>
                                         <td className="border-s-4">{d.website}</td>
                                         <td className="border-s-4">
-                                            <button className="px-5 bg-black text-white me-5 font-semibold p-1">Edit</button>
+                                        <button className="px-5 bg-blue-400 text-white me-5 font-semibold p-1">Read</button>
+                                            <button className="px-5 bg-blue-600 text-white me-5 font-semibold p-1">Edit</button>
                                             <button className="bg-red-600 text-white px-5 font-semibold p-1">Delete</button>
                                         </td>
-                                        
-
                                     </tr>
                                    ))
                                 }
                             </tbody>
                         </table>
-                </div>
+                     </div>
             </div>
         </>
     );
 }
-export default Home;
+export default Home;    
